1f1b850b577e639544e0128f77ca37c2858afa9d
72257910+Mikan-DS@users.noreply.github.com <72257910+Mikan-DS@users.noreply.github.com>
2023-04-24 20:57:37 +0300

    Project

---
+++ AreaFinder/AreaFinder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AreaFinder
+{
+    
+    public interface IFigure
+    {
+        double GetArea();
+    }
+
+    
+    
+    public class Circle : IFigure
+    {
+        private readonly double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+    }
+
+    public class Triangle : IFigure
+    {
+        private readonly double _sideA;
+        private readonly double _sideB;
+        private readonly double _sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+
+        public double GetArea()
+        {
+            double p = (_sideA + _sideB + _sideC) / 2;
+            return Math.Sqrt(p * (p - _sideA) * (p - _sideB) * (p - _sideC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = new double[] { _sideA, _sideB, _sideC };
+            Array.Sort(sides);
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+
+    
+    
+}
---
+++ AreaFinder/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AreaFinder.Tests
+{
+    [TestFixture]
+    public class FigureTests
+    {
+        [Test]
+        public void TestCircleArea()
+        {
+            double radius = 5;
+            Circle circle = new Circle(radius);
+            Assert.AreEqual(Math.PI * Math.Pow(radius, 2), circle.GetArea());
+        }
+
+        [Test]
+        public void TestTriangleArea()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            Assert.AreEqual(6, triangle.GetArea());
+        }
+
+        [Test]
+        public void TestRightTriangle()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            Assert.IsTrue(triangle.IsRightTriangle());
+        }
+    }
+
+}
